b8cb6e4bb49381c0b14e15f415cb02dd082795b2
fernanda oliveira <fer.nand.2149@bol.com.br>
2021-07-29 17:27:43 -0300

    Finalização index

---
+++ index.js
@@ -0,0 +1,49 @@
+import "dotenv/config";
+import Discord from "discord.js";
+import YTDL from "ytdl-core";
+
+const url = "https://www.youtube.com/watch?v=MaCR_tWXScg&t=3101s";
+const channelId = "858420360441364500";
+let playing = false;
+
+const client = new Discord.Client({
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
+
+function play(connection) {
+  playing = true;
+  const stream = YTDL(url, { filter: "audioonly" });
+  connection.play(stream, { volume: 1, seek: 0 });
+}
+
+async function replay() {
+  const voiceChannel = await client.channels.fetch(channelId);
+  if (voiceChannel) {
+    voiceChannel
+      .join()
+      .then((connection) => {
+        if (!playing) {
+          play(connection);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        playing = false;
+      });
+  }
+}
+
+client.on("ready", async () => {
+  console.log("Codefi on");
+  replay();
+});
+
+client.on("message", (message) => {
+  if (message.content === "<3play") {
+    if (!playing) {
+      replay();
+    }
+  }
+});
+
+client.login(process.env.token);
